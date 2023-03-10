291fc8e1288eb277bf9411b126b601b4c40bc34d
Kiaran Lau <76144488+pluffpenguin@users.noreply.github.com>
2023-03-10 14:16:24 -0800

    client socket established

--- sockets/client_socket.py
+++ sockets/client_socket.py
@@ -1,19 +1,41 @@
 import socket
 
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 HOSTNAME = socket.gethostname()
 IP_ADDRESS = socket.gethostbyname(HOSTNAME)
 PORT = 1234
 
-print(HOSTNAME, IP_ADDRESS)
+ADDR = (IP_ADDRESS, PORT)
 
-# We need to create UDP Socket communication
+client = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+client.connect(ADDR)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print(f"Client Started: {HOSTNAME}, {ADDR}")
 
-# bind the socket to the IP_ADDRESS and PORT
-sock.bind((IP_ADDRESS, PORT))
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    
+send("Hello World!")
+    
 
-# wait for incoming messages
-while True:
-    data, address = sock.recvfrom(1024)  # buffer size is 1024 bytes
-    print(f"Received message: {data.decode()} from {address[0]}:{address[1]}")
+# print(HOSTNAME, IP_ADDRESS)
+
+# # We need to create UDP Socket communication
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# # bind the socket to the IP_ADDRESS and PORT
+# sock.bind((IP_ADDRESS, PORT))
+
+# # wait for incoming messages
+# while True:
+#     data, address = sock.recvfrom(1024)  # buffer size is 1024 bytes
+#     print(f"Received message: {data.decode()} from {address[0]}:{address[1]}")
