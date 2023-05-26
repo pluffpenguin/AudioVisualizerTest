3c4b31eb0b511aaf69cdc5a59bfc002c299ccbb3
pluffpenguin <roboperson10@gmail.com>
2023-05-25 23:18:33 -0700

    deprecated fade

--- sockets/client_socket.py
+++ sockets/client_socket.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 from moduleLedColor import ModuleLed
 
@@ -15,6 +16,13 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
 led_strip = ModuleLed()
+currentColor = [0, 0, 0]
+targetColor = [0, 0, 0]
+
+step = [0, 0, 0]
+numSteps = 10
+stepTime = 1
+sleepTime = stepTime/numSteps
 
 def parseColorInfo(new_msg):
     new_msg = new_msg[2:]
@@ -23,7 +31,19 @@ def parseColorInfo(new_msg):
         new_msg[i] = int(new_msg[i])
     return new_msg
 
+def calculateColorStep():
+    step[0] = (targetColor[0] - currentColor[0])/numSteps
+    step[1] = (targetColor[1] - currentColor[1])/numSteps
+    step[2] = (targetColor[2] - currentColor[2])/numSteps
+
+def calculateNewCurrentColor():
+    currentColor[0] = currentColor[0] - step[0]
+    currentColor[1] = currentColor[1] - step[1]
+    currentColor[2] = currentColor[2] - step[2]
+
 def send(msg):
+    global targetColor
+    global currentColor
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
@@ -35,9 +55,17 @@ def send(msg):
     if new_msg[0] == 'C':
         new_msg = parseColorInfo(new_msg)
         led_strip.setColor(new_msg)
+        # targetColor = new_msg
+        # calculateColorStep()
+        # while (currentColor[0] - targetColor[0]) > .1:
+        #     calculateNewCurrentColor()
+        #     print(currentColor)
+        #     sentColor = [int(currentColor[0]), int(currentColor[1]), int(currentColor[2])]
+        #     led_strip.setColor(sentColor)
+        #     time.sleep(sleepTime)
+        # currentColor = targetColor
 
-
-
+        
 target_input = input()
 while target_input != DISCONNECT_MESSAGE:
     send(target_input)
